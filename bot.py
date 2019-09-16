f3ed6b08082933be8d5b19143d03c665d8a6e404
Yuri Astafev <tyler333@mail.ru>
2019-09-17 00:07:11 +0300

    ephem

--- bot.py
+++ bot.py
@@ -1,11 +1,27 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+from datetime import datetime, date, time
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
+
+now = datetime.now()
+
+def const(bot, update):
+    user_text = update.message.text
+    user_text_list = user_text.split()
+    planet = user_text_list[1].lower()
+    if planet == 'mars':
+        mars = ephem.Mars(f'{now.year}/{now.month}/{now.day}')
+        print(ephem.constellation(mars))
+        update.message.reply_text(f'Планета находится в созвездии: {ephem.constellation(mars)}')
+    else:
+        print("Не знаю такой планеты :(")
+        update.message.reply_text("Не знаю такой планеты :(")
 
 def greet_user(bot, update):
     user_text = 'Вызван /start'
@@ -26,6 +42,7 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", const))
 
 
     mybot.start_polling()
